f1d17a74d8fea02cf78c18d4b809d149988f3a96
Santiago Mauhourat <smauhourat@aadim.org.ar>
2024-01-04 17:19:42 -0300

    async api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -16,17 +17,17 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = _context.Users.ToList();
+        var users = await _context.Users.ToListAsync();
 
         return users;
     }
 
     [HttpGet("{id}")]
-    public ActionResult<AppUser> Get(int id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        var user = _context.Users.Find(id);
+        var user = await _context.Users.FindAsync(id);
 
         return user;
     }
