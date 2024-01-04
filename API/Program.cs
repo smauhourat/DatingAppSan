5a2ef222057b87469f2e2549b239ff1c69b35c06
Santiago Mauhourat <smauhourat@aadim.org.ar>
2024-01-04 16:09:01 -0300

    add users api

--- API/Program.cs
+++ API/Program.cs
@@ -7,11 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
