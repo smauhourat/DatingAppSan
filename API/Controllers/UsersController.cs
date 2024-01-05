240115c3b31986ef105265c63b0a0a7cf438fffd
SMauhourat <santiagomauhourat@hotmail.com>
2024-01-04 22:47:02 -0300

    htto ang

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
 
