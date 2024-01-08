e96e3d10a1d56c0f31955d93c9e8cf5faaadb1eb
Santiago Mauhourat <smauhourat@aadim.org.ar>
2024-01-08 14:14:00 -0300

    jwt

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using API.Entities;
+
+namespace API;
+
+public class TokenService : ITokenService
+{
+
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
